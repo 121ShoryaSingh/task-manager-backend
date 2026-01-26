8dc0dea0e96d7f2fcf6c2c1fb020a1778f32aaa8
121ShoryaSingh <shoryasingh810@gmail.com>
2026-01-26 16:48:30 +0530

    creating a class for Api Response

---
+++ src/utils/ApiResponse.ts
@@ -0,0 +1,22 @@
+type ApiResponseTypes = {
+  statusCode: number;
+  data: object;
+  message: string;
+  success: boolean;
+};
+
+class ApiResponse implements ApiResponseTypes {
+  statusCode: number;
+  data: object;
+  message: string;
+  success: boolean;
+
+  constructor(statusCode: number, data: object, message: string = "Success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}
+
+export { ApiResponse };
