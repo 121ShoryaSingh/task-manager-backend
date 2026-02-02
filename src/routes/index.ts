413cb5e9a7ff3bd49a57b9e40780243055b39f29
121ShoryaSingh <shoryasingh810@gmail.com>
2026-02-02 12:02:42 +0530

    adding register route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import healthRoutes from "../routes/healthcheck.routes.js";
+import authRoutes from "../routes/auth.routes.js";
 
 const router = Router();
 
 router.use("/health", healthRoutes);
+router.use("/auth", authRoutes);
 
 export default router;
