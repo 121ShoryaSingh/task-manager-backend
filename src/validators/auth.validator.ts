14f8f8e27741ce7caa0a06644ddc08436a1e2254
121ShoryaSingh <shoryasingh810@gmail.com>
2026-02-03 13:00:24 +0530

    validation schema

---
+++ src/validators/auth.validator.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  username: z.string().min(3, "Username must be at least 3 character"),
+  email: z.email("Invalid email address"),
+  fullName: z.string().min(1, "Full name is required"),
+  password: z.string().min(8, "Password must be at least 8 character"),
+});
