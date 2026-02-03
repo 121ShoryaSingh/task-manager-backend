90e7bed89cf58beffe08f576d757e4b5de498623
121ShoryaSingh <shoryasingh810@gmail.com>
2026-02-03 13:01:02 +0530

    adding middleware to route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/auth.controllers.js";
+import { validateRequest } from "../middlewares/validator.middleware.js";
+import { registerSchema } from "../validators/auth.validator.js";
 
 const router = Router();
 
-router.post("/register", registerUser);
+router.post("/register", validateRequest(registerSchema), registerUser);
 
 export default router;
