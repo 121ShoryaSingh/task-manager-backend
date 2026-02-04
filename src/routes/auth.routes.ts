e8695d27f704c56e6e839eb54de07ec242d26864
121ShoryaSingh <shoryasingh810@gmail.com>
2026-02-04 13:36:22 +0530

    added login route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/auth.controllers.js";
+import { login, registerUser } from "../controllers/auth.controllers.js";
 import { validateRequest } from "../middlewares/validator.middleware.js";
-import { registerSchema } from "../validators/auth.validator.js";
+import { loginSchema, registerSchema } from "../validators/auth.validator.js";
 
 const router = Router();
 
 router.post("/register", validateRequest(registerSchema), registerUser);
+router.post("/login", validateRequest(loginSchema), login);
 
 export default router;
