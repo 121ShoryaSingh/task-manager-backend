63aa0736cd61eeeabdab3edd31374a1789765cf9
121ShoryaSingh <shoryasingh810@gmail.com>
2026-02-04 13:36:12 +0530

    created a login schema for validation

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -6,3 +6,8 @@ export const registerSchema = z.object({
   fullName: z.string().min(1, "Full name is required"),
   password: z.string().min(8, "Password must be at least 8 character"),
 });
+
+export const loginSchema = z.object({
+  email: z.email("Invalid email address"),
+  password: z.string().min(8, "Password must be at least 8 character"),
+});
