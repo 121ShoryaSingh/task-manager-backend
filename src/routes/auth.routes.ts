857b5544fe8ab3ba0a592c297aeae807a4f5aff8
121ShoryaSingh <shoryasingh810@gmail.com>
2026-02-05 12:44:41 +0530

    creating a route for logout

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { login, registerUser } from "../controllers/auth.controllers.js";
+import {
+  login,
+  logout,
+  registerUser,
+} from "../controllers/auth.controllers.js";
 import { validateRequest } from "../middlewares/validator.middleware.js";
 import { loginSchema, registerSchema } from "../validators/auth.validator.js";
+import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/register", validateRequest(registerSchema), registerUser);
 router.post("/login", validateRequest(loginSchema), login);
+router.post("/logout", verifyToken, logout);
 
 export default router;
